11bb5a962c3e54c628c8748278b5cc43f1c11416
FPaurisse <paurissefrederic@gmail.com>
2020-02-02 20:40:58 +0100

    delete headers control

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(
 app.use(cors());
 app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.get('/api/v1/', async (req, res) => {
     const message = await 'Hello World!';
     res.send(message);
