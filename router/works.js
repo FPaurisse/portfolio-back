7214450ac7abbb5d0b00e1ccd35beff3f5f394da
FPaurisse <paurissefrederic@gmail.com>
2020-02-15 18:02:06 +0100

    add work data

--- router/works.js
+++ router/works.js
@@ -4,7 +4,7 @@ const slugify = require('slugify');
 const router = express.Router();
 
 const works = [
-    {   
+    {
         "id": 1,
         "slug": slugify("Développement web", { lower: true }),
         "title": "Développement web",
@@ -22,12 +22,12 @@ const works = [
     },
     {
         "id": 3,
-        "slug": slugify("Troisième titre bis", { lower: true }),
-        "title": "Troisième titre bis",
+        "slug": slugify("Second titre", { lower: true }),
+        "title": "Second titre",
         "context": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
         "tools": ["JavaScript", "Node.js"],
         "categories": ["web"]
-    }
+    },
 ];
 
 router.get('/', async (req, res) => {
