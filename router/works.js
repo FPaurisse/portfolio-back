e5815aa6e32ee973464a1f689ff1df9b987701e2
FPaurisse <paurissefrederic@gmail.com>
2020-02-16 08:03:07 +0100

    refactoring

--- router/works.js
+++ router/works.js
@@ -4,33 +4,33 @@ const slugify = require('slugify');
 const router = express.Router();
 
 const works = [
-    {
-        "id": 1,
-        "slug": slugify("Développement web", { lower: true }),
-        "title": "Développement web",
-        "context": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-        "tools": ["JavaScript", "React.js"],
-        "categories": ["web"]
-    },
-    {
-        "id": 2,
-        "slug": slugify("Second titre", { lower: true }),
-        "title": "Second titre",
-        "context": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-        "tools": ["JavaScript", "React.js"],
-        "categories": ["print"]
-    },
-    {
-        "id": 3,
-        "slug": slugify("Dernier titre", { lower: true }),
-        "title": "Dernier titre",
-        "context": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-        "tools": ["JavaScript", "Node.js"],
-        "categories": ["web"]
-    },
+  {
+    id: 1,
+    slug: slugify('Développement web', { lower: true }),
+    title: 'Développement web',
+    context: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+    tools: ['JavaScript', 'React.js'],
+    categories: ['web'],
+  },
+  {
+    id: 2,
+    slug: slugify('Second titre', { lower: true }),
+    title: 'Second titre',
+    context: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+    tools: ['JavaScript', 'React.js'],
+    categories: ['print'],
+  },
+  {
+    id: 3,
+    slug: slugify('Dernier titre', { lower: true }),
+    title: 'Dernier titre',
+    context: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+    tools: ['JavaScript', 'Node.js'],
+    categories: ['web'],
+  },
 ];
 
 router.get('/', async (req, res) => {
-    res.send(works);
-})
+  res.send(works);
+});
 module.exports = router;
