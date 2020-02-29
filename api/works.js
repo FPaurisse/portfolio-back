fbf70222af01fb7fb240d4d036c49131b059c51b
FPaurisse <paurissefrederic@gmail.com>
2020-02-29 21:27:17 +0100

    add route work/:slug

--- api/works.js
+++ api/works.js
@@ -1,26 +1,62 @@
 const express = require('express');
 const slugify = require('slugify');
+const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
 
 const router = express.Router();
 const Work = require('../shemas/Work.model');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}`);
+  },
+});
+
+const upload = multer({ storage });
 
 router.get('/', async (req, res) => {
   const works = await Work.find();
   res.json(works);
 });
 
-router.post('/', async (req, res) => {
+router.get('/:slug', async (req, res) => {
+  const { slug } = req.params;
+  const work = await Work.find({ slug });
+  res.json(work);
+});
+
+router.post('/', upload.any(), async (req, res) => {
+  const { files } = req;
+  req.files.map(async (file) => {
+    await sharp(file.path)
+      .resize(800, 800, {
+        fit: sharp.fit.inside,
+        withoutEnlargement: true,
+        progressive: true,
+      })
+      .toFormat('png')
+      .toFile(`public/uploads/${file.filename}.png`);
+    fs.unlink(`public/uploads/${file.filename}`, (err) => {
+      if (err) throw err;
+    });
+  });
+  const image = await files.find((file) => file.fieldname.includes('image'));
+  const mockup = await files.find((file) => file.fieldname.includes('mockup'));
   const {
-    title, image, primaryColor, secondaryColor, optionalColor, mockup, context, tools, categories,
+    title, primaryColor, secondaryColor, optionalColor, context, tools, categories,
   } = req.body;
   const work = new Work({
-    slug: slugify(title, { lower: true }),
     title,
-    image,
+    slug: slugify(title, { lower: true }),
+    image: image.path.replace('public/uploads', 'uploads').concat('.png'),
     primaryColor,
     secondaryColor,
     optionalColor,
-    mockup,
+    mockup: mockup.path.replace('public/uploads', 'uploads').concat('.png'),
     context,
     tools: tools.split(/\s*,\s*/).map((tool) => tool.charAt(0).toUpperCase() + tool.slice(1)),
     categories: categories.split(/\s*,\s*/).map((category) => category.charAt(0).toUpperCase() + category.slice(1)),
@@ -29,35 +65,6 @@ router.post('/', async (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   await Work.findByIdAndUpdate({ _id: work._id }, { id: work._id });
   res.send(work);
-});
-
-// DATA TEST
-const example = [
-  {
-    title: 'Premier titre',
-    context: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    tools: 'React.js, Node.js',
-    categories: 'web',
-  },
-];
-
-router.get('/create', async (req, res) => {
-  const work = new Work({
-    slug: slugify(example[0].title, { lower: true }),
-    title: example[0].title,
-    context: example[0].context,
-    tools: example[0].tools.split(/\s*,\s*/).map((tool) => tool.charAt(0).toUpperCase() + tool.slice(1)),
-    categories: example[0].categories.split(/\s*,\s*/).map((category) => category.charAt(0).toUpperCase() + category.slice(1)),
-  });
-  await work.save();
-  // eslint-disable-next-line no-underscore-dangle
-  await Work.findByIdAndUpdate({ _id: work._id }, { id: work._id });
-  res.send(work);
-});
-
-router.get('/delete', async (req, res) => {
-  await Work.deleteOne();
-  res.send('Example deleted');
 });
 
 module.exports = router;
