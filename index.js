a60fbae791ed96af0598ece2c3e40f4948480800
FPaurisse <paurissefrederic@gmail.com>
2020-03-01 11:35:20 +0100

    test cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
