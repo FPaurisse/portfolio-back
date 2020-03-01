269a6660c99e06f07cd3571c04266968081c0d66
FPaurisse <paurissefrederic@gmail.com>
2020-03-01 12:51:10 +0100

    test cors

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ app.use(
     extended: true,
   }),
 );
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'admin.fredericpaurisse.fr'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 app.use(express.static('public'));
 
