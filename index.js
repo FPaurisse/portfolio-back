759c0b1694956993099e5f0dface8a0b2fb2fe4b
FPaurisse <paurissefrederic@gmail.com>
2020-03-01 11:37:25 +0100

    test cors

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ app.use(
   }),
 );
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(express.static('public'));
 
 app.use('/api/v1/works', require('./api/works'));
